d58adcda0f4312e8cd422e74b2579516e9a1b2bd
garvitinfo-paresh <garvitinfo.paresh@gmail.com>
2024-08-13 09:13:34 +0530

    13-08

---
+++ 31mathFunc.php
@@ -0,0 +1,125 @@
+<?php
+
+// $x = 5985;
+// echo gettype($x);
+
+// $x = "5985";
+// echo gettype($x);
+
+// $x = "59.85" + 100;
+// echo gettype($x);
+
+// $x = "Hello";
+// echo gettype($x);
+
+// Cast float to int
+// $x = 23465.768;
+// $int_cast = (int)$x;
+// echo $int_cast;
+
+// Cast string to int
+// $x = "23465.768";
+// $x = "ABC";
+// $int_cast = (int)$x;
+// echo $int_cast;
+
+
+// abs — Absolute value
+// echo (abs(-4.2)) ;
+// echo (abs(-5)) ;
+// echo (abs(5));
+// ----
+
+// floor — Round fractions down
+// echo floor(4.3) ; 
+// echo floor(9.999) ;
+// echo floor(-3.14);  
+// ---
+
+// ceil — Round fractions up
+// echo ceil(4.3) ; 
+// echo ceil(9.999) ;
+// echo ceil(-3.14);  
+// ----
+
+// round — Rounds a float
+// echo round(4.3) ; 
+// echo round(9.999) ;
+// echo round(-3.14);  
+// ----
+
+// decbin — Decimal to binary
+// echo decbin(12)
+// echo decbin(26);
+// ----
+
+// dechex — Decimal to hexadecimal
+// echo dechex(12);
+// echo dechex(26);
+// ----
+
+// decoct — Decimal to octal
+// echo decoct(13);
+// echo decoct(26);
+// ----
+
+// hexdec — Hexadecimal to decimal
+// echo hexdec('FF');
+// echo hexdec('0A');
+// echo hexdec('A0');
+// ----
+
+// octdec — Octal to decimal
+// echo octdec('10');
+// echo octdec('100');
+// echo octdec('1000');
+// ----
+
+// fdiv — Divides two numbers, according to IEEE 754
+// echo fdiv(10,3);
+// echo fdiv(4, 2); 
+// echo fdiv(1.0, 0.0); 
+// echo fdiv(-1.0, 0.0); 
+// echo fdiv(0.0, 0.0); 
+// ----
+
+// intdiv — Integer division
+// echo intdiv(3, 2);
+// echo intdiv(-3, 2);
+// echo intdiv(3, -2);
+// echo intdiv(-3, -2);
+
+// fmod — Returns the floating point remainder (modulo) of the division of the arguments
+// echo fmod(10,3);
+// echo fmod(80,3);
+// echo fmod(90,9);
+// ----
+
+// max — Find highest value
+// echo max(10,20,30);
+// echo max(30,20,10);
+// echo max(10,30,20);
+// ----
+
+// min — Find lowest value
+// echo min(10,20,30);
+// echo min(30,20,10);
+// echo min(10,30,20);
+// ----
+
+// pi — Get value of pi
+// echo pi();
+// ----
+
+// pow — Exponential expression
+// echo pow(5,3);
+// echo pow(3,5);
+// ----
+
+// sqrt — Square root
+// echo sqrt(25);
+// echo sqrt(9);
+// echo sqrt(2);
+// ----
+
+?>
